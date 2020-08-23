512a4a82425056d41fad493ef2a172aaf24e7e75
LironBayda <34834883+LironBayda@users.noreply.github.com>
2020-08-23 21:19:06 +0300

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car19082020
+{
+    public class Car
+    {
+        public int Model { get; set; }
+        public string Brand { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+        private int _codan;
+        protected int _numberOfSteas;
+        public Car()
+        {
+
+        }
+
+        public Car(int model, string brand, int year, string color, int codan, int numberOfSteas)
+        {
+            Model = model;
+            Brand = brand;
+            Year = year;
+            Color = color;
+            _codan = codan;
+            _numberOfSteas = numberOfSteas;
+        }
+
+        public int GetCoden()
+        {
+            return _codan;
+        }
+
+        public int GetNumberOfSeats()
+        {
+            return _numberOfSteas;
+        }
+
+        protected void changeNumberOfSeats(int numberOfSeats)
+        {
+            _numberOfSteas = numberOfSeats;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() +$"{ Year} { Brand} { Model} { Color} {_codan} {_numberOfSteas}";
+        }
+
+
+        public Car(string fileName)
+        {
+            XmlSerializer myXmlSerializer = new XmlSerializer(typeof(Car));
+
+            /*   Car c1 = new Car(5564,"kkk",2,"red",6,5);
+
+                 using (Stream file = new FileStream(@"../../xmlfile.xml", FileMode.Create)) // creating new file stream
+                 {
+                     myXmlSerializer.Serialize(file, c1);
+
+                 } //closing file stream
+                */
+            Car c;
+
+            //XmlSerializer myXmlSerializer = new XmlSerializer(typeof(Person));
+
+            using (Stream file = new FileStream(@"../../xmlfile.xml", FileMode.Open)) // creating new file stream
+            {
+                c = myXmlSerializer.Deserialize(file) as Car;
+
+            } //closing file stream
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Car19082020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlSerializer myXmlSerializer = new XmlSerializer(typeof(Car));
+
+            /*   Car c1 = new Car(5564,"kkk",2,"red",6,5);
+
+                 using (Stream file = new FileStream(@"../../xmlfile.xml", FileMode.Create)) // creating new file stream
+                 {
+                     myXmlSerializer.Serialize(file, c1);
+
+                 } //closing file stream
+                */ 
+            Car c2;
+
+            //XmlSerializer myXmlSerializer = new XmlSerializer(typeof(Person));
+
+            using (Stream file = new FileStream(@"../../xmlfile.xml", FileMode.Open)) // creating new file stream
+            {
+                c2 = myXmlSerializer.Deserialize(file) as Car;
+
+            } //closing file stream
+
+            //Console.WriteLine(c1.ToString());
+            Console.WriteLine(c2.ToString());
+            Console.ReadLine();
+
+            // --------------------- Array
+            Car[] cArray =
+            {
+                new Car(5564,"kkk",2,"red",6,58),
+            new Car(5564, "vvv", 27, "red", 67, 5),
+            new Car(5564, "ddd", 62, "red", 6, 55),
+            new Car(5564, "sss", 28, "red", 66, 5),
+            };
+
+            XmlSerializer myXmlSerializerArray = new XmlSerializer(typeof(Car[]));
+            using (Stream file = new FileStream(@"../../xmlarrayfile.xml", FileMode.Create)) // creating new file stream
+            {
+                myXmlSerializerArray.Serialize(file, cArray);
+            } //closing file stream
+
+            Car[] p2Array;
+            using (Stream file = new FileStream(@"../../xmlarrayfile.xml", FileMode.Open)) // creating new file stream
+            {
+                p2Array = myXmlSerializerArray.Deserialize(file) as Car[];
+            } //closing file stream
+
+
+
+        }
+
+
+
+    }
+}
